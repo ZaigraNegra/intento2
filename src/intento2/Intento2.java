10c2de02d41243ed20eb9cfd8d03ec94e6730b67
Said Nuñez <Said Nuez@10.1.9.13>
2022-11-17 15:42:42 -0700

    este es el intento 2

---
+++ src/intento2/Intento2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package intento2;
+
+/**
+ *
+ * @author Said Nuñez
+ */
+public class Intento2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Este es el intento numero 2");
+    }
+    
+}
